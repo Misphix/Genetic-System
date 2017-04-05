18183f05971fbf43125a621597f200386c46c40d
Miphix <bcspcjddof@gmail.com>
2017-04-05 17:04:15 +0800

    Add genetic system UI

--- src/Main.java
+++ src/Main.java
@@ -4,36 +4,79 @@ import GeneticSystem.GeneticSystem;
 import GeneticSystem.DNA;
 import Util.FileUtility;
 import Constants.Constants;
+import javafx.application.Application;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.scene.control.TextArea;
+import javafx.stage.Stage;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 
 import java.io.IOException;
 
-public class Main {
-    public static void main(String[] args) {
-        int neuronNumber = 7;
-        GeneticParameter geneticParameter = new GeneticParameter();
-        geneticParameter.setCrossOverProbability("0.6");
-        geneticParameter.setFitValueThreshold("2.5");
-        geneticParameter.setMutationProbability("0.7");
-        geneticParameter.setPopulationSize("500");
-        geneticParameter.setIterationCount("50");
+public class Main extends Application {
+    @FXML
+    TextField population, crossover, mutation, neuronSize, maxIteration, accept;
+    @FXML
+    TextArea result;
+    @FXML
+    Button start;
 
-        GeneticSystem geneticSystem = new GeneticSystem(Constants.RBF_DEFAULT_DIMENSION,geneticParameter, new RBF(neuronNumber));
-
+    @Override
+    public void start(Stage primaryStage) {
         try {
-            geneticSystem.loadTrainingData(FileUtility.getLines(FileUtility.getFileName("data")));
-        } catch (IOException ex) {
-            ex.printStackTrace();
+            Parent root = FXMLLoader.load(getClass().getResource("Ui.fxml"));
+            Scene scene = new Scene(root, 600, 500);
+            primaryStage.setTitle("RBFN");
+            primaryStage.setScene(scene);
+            primaryStage.show();
+        } catch (Exception e) {
+            e.printStackTrace();
         }
-        geneticSystem.run();
-        RBF rbf = new RBF(neuronNumber);
-        rbf.setParameter(
-                geneticSystem.getSolutionDNA().getTheta(),
-                geneticSystem.getSolutionDNA().getWeights(),
-                geneticSystem.getSolutionDNA().getDistances(),
-                geneticSystem.getSolutionDNA().getSigma()
-        );
-        System.out.println();
-        DNA solution = geneticSystem.getSolutionDNA();
-        System.out.println(solution);
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    public void compute() {
+        result.clear();
+        start.setDisable(true);
+
+        new Thread(() -> {
+            int neuronNumber = Integer.valueOf(neuronSize.getText());
+
+            GeneticParameter geneticParameter = new GeneticParameter();
+            geneticParameter.setCrossOverProbability(crossover.getText());
+            geneticParameter.setFitValueThreshold(accept.getText());
+            geneticParameter.setMutationProbability(mutation.getText());
+            geneticParameter.setPopulationSize(population.getText());
+            geneticParameter.setIterationCount(maxIteration.getText());
+
+            GeneticSystem geneticSystem = new GeneticSystem(Constants.RBF_DEFAULT_DIMENSION, geneticParameter, new RBF(neuronNumber));
+
+            try {
+                geneticSystem.loadTrainingData(FileUtility.getLines(FileUtility.getFileName("data")));
+            } catch (IOException ex) {
+                ex.printStackTrace();
+            }
+            geneticSystem.run();
+            RBF rbf = new RBF(neuronNumber);
+            rbf.setParameter(
+                    geneticSystem.getSolutionDNA().getTheta(),
+                    geneticSystem.getSolutionDNA().getWeights(),
+                    geneticSystem.getSolutionDNA().getDistances(),
+                    geneticSystem.getSolutionDNA().getSigma()
+            );
+            System.out.println();
+            DNA solution = geneticSystem.getSolutionDNA();
+
+            String output = String.format("Fitness value: %.10f\n", solution.getFitnessVaule());
+            output += solution.toString();
+            result.setText(output);
+            start.setDisable(false);
+        }).start();
     }
 }
